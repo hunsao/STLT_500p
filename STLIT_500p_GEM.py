b06693fb0f75973842d0c125f8998d91dd4de606
David C. <84016055+hunsao@users.noreply.github.com>
2025-05-09 18:09:54 +0200

    Update STLIT_500p_GEM.py

--- STLIT_500p_GEM.py
+++ STLIT_500p_GEM.py
@@ -11,11 +11,13 @@ import base64
 import time
 
 from st_aggrid import AgGrid
+# from streamlit import cache_data # Deprecated, use st.cache_data
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload, build_http # build_http para el servicio
+from googleapiclient.http import MediaIoBaseDownload
+# from google_auth_httplib2 import Request # Not explicitly used, http is built directly
 from googleapiclient.errors import HttpError
-from google_auth_httplib2 import AuthorizedHttp # Simplificaremos esto
+from google_auth_httplib2 import AuthorizedHttp # Asegúrate de que esta librería esté instalada
 
 st.set_page_config(layout="wide")
 
